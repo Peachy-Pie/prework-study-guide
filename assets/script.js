0f78567607f9794dcffd08f2b2e7e80a76e43c0d
Peachy-Pie <patrick.e.straw@gmail.com>
2022-10-09 12:35:06 -0700

    added JS notes to prework-study-guide

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,39 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-for (var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+function listtopicss() {
+
+    for (var x = 0; x < topics.length; x++) {
+
+        console.log(topics[x]);
+    }
 }
 
-/*
-if (topic === 'HTML') {
+function selecttopics() {
+
+    if (randomTopic === 'HTML') {
     console.log("let's study HTML");
 
-} else if (topic === 'CSS') {
+    } else if (randomTopic === 'CSS') {
     console.log("let's study CSS");
 
-} else if (topic === 'Git') {
+    } else if (randomTopic === 'Git') {
     console.log("let's study Git!");
 
-} else if (topic === 'JavaScript') {
+    } else if (randomTopic === 'JavaScript') {
     console.log("let's study LavaScript!")
 
-} else {
+    } else {
     console.log("please try again");
-}
-*/
+    
+    }
 
+}
+console.log('Here are the topics we learned through Prework:');
+
+listtopicss();
+
+console.log('which topic should we study first?');
+
+selecttopics();
